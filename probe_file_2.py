b6f9f822510586c01db535b27f60ba3738e345a6
Олег <oleg-kayurov@mail.ru>
2020-06-09 13:42:41 +0300

    смотри 3 файла

---
+++ probe_file_2.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+#import pprint
+import datetime
+
+
+def adress(html):
+    response = requests.get(html)
+    html = BeautifulSoup(response.text, 'html.parser')
+    list_v = html.find_all('a', {'class': 'home-link home-link_black_yes inline-stocks__link'})
+    list_f = html.find_all('span', {'class': 'inline-stocks__value_inner'})
+    for k, v in zip(list_f, list_v):
+        print(k.text, v.text)
+        #pprint.pprint(k.text, v.text)
+
+def main():
+    adress('https://yandex.ru/')
+
+
+if __name__ == '__main__':
+    main()
+
