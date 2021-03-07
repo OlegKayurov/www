5e8be6cea306a82d1292bb94e5d952f824314435
Professional <369QWE258asd>
2021-03-07 22:33:53 +0300

    Pars

--- probe_file.py
+++ probe_file.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-
+# prob
 # Currency parser
 def adress(html):
-    response = requests.get(html)
-    html = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get(html).text
+    html = BeautifulSoup(response, 'lxml')
     list_v = html.find_all('a', {'class': 'home-link home-link_black_yes inline-stocks__link'})
     list_f = html.find_all('span', {'class': 'inline-stocks__value_inner'})
     for k, v in zip(list_f, list_v):
