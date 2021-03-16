aa212413b74ed8dda942b9d61db7878cfc87b4e5
Professional <369QWE258asd>
2021-03-16 21:41:27 +0300

    v0.2 добавлена функция загрузки картинки и функция изменения разрешения картинки

--- parser_wb_func.py
+++ parser_wb_func.py
@@ -1,10 +1,15 @@
 import csv
+import os, sys
+import time
+from PIL import Image
 import requests
 from bs4 import BeautifulSoup as bs
 import mysql.connector as sql
 import string
 import pandas as pd
 
+path = "D:\python1\image/"
+dirs = os.listdir(path)
 
 #добавление записей в БД
 def save(cur, con, link, brend1, form_price):
@@ -43,8 +48,6 @@ def filtr(cur):
     # Clean up
     f.close()
 
-
-
 #запись данных в csv файл
 def save_scv(cur, con):
     f = open('save_all_data.csv', 'w')
@@ -59,6 +62,46 @@ def save_scv(cur, con):
     cur.close()
     con.commit()
 
+#Перезаписываем сохраненную картинку с нужными пикселями
+def resize(path, dirs):
+    for item in dirs:
+        im = Image.open(path+item)
+        f, e = os.path.splitext(path+item)
+        imResize = im.resize((100,100), Image.ANTIALIAS)
+        imResize.save(f + ' resized.jpg', 'JPEG', quality=90)
+
+#Загрузка картинки
+def download():
+    number = 0
+    c = 'images'
+    url = 'https://www.wildberries.ru/catalog/obuv/muzhskaya/kedy-i-krossovki'
+    r = requests.get(url).text
+    soup = bs(r, 'lxml')
+    links = soup.find('div', class_='catalog_main_table j-products-container')
+    all_links = links.find_all('div', class_='dtList i-dtList j-card-item')
+
+    for i in all_links:
+        brend = i.find('strong', class_='brand-name c-text-sm').text
+        brend1 = brend.replace('/', '').replace(' ', '')
+        links_image = i.find('img').get('src')
+        links_image2 = i.find('img').get('data-original')
+        if c in links_image:
+                number += 1
+                lin1 = links_image
+                print(lin1)
+                r = requests.get(f"{'https:'}{lin1}").content
+                time.sleep(1)
+                with open(f"{'image/'}{number}{brend1}.jpg", 'wb') as f:
+                    f.write(r)
+
+        if links_image2 is not None:
+            number += 1
+            lin = links_image2
+            r = requests.get(f"{'https:'}{lin}").content
+            time.sleep(1)
+            r2 = f'{number}{brend1}'
+            with open(f"{'image/'}{number}{brend1}.jpg", 'wb') as f:
+                f.write(r)
 
 def main():
     con = sql.connect(host='localhost', user='root', password='root', database='datapars')
@@ -77,6 +120,8 @@ def main():
                     '"View full list" - F\n'
                     '"Filtr" - I\n'
                     '"Save to Excel" - E\n'
+                    '"Save Image" - S\n'
+                    '"Resize" - R\n'
                     '"Exit" - Q\n').lower()
         if act == 'a':
             while True:
@@ -90,7 +135,9 @@ def main():
                     brend = i.find('strong', class_='brand-name c-text-sm').text
                     brend1 = brend.replace('/', '').replace(' ', '')
                     price = i.find('span', class_='price').text
-                    #price2 = price.replace('\n', '')
+                    links_image = i.find('img').get('src')
+                    links_image1 = f"{'https:'}{i.find('img').get('data-original')}"
+                    links_image2 = i.find('img').get('data-original')
                     valid_characters = string.printable
                     start_string = f"{price}"
                     price2 = ''.join(i for i in start_string if i in valid_characters).replace('\n', '')
@@ -114,8 +161,14 @@ def main():
                         if act == 'e':
                             save_scv(cur, con)
                         else:
-                            if act == 'q':
-                                break
+                            if act == 's':
+                                download()
+                            else:
+                                if act == 'r':
+                                    resize(path, dirs)
+                                else:
+                                    if act == 'q':
+                                        break
 
 if __name__=='__main__':
     main()
